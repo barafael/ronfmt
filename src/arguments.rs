ae446d599b6934b49a0ae16b9eea8057def58432
Rafael Bachmann <rafael.bachmann@esrlabs.com>
2023-03-09 22:22:29 +0100

    Migrate to clap 4

---
+++ src/arguments.rs
@@ -0,0 +1,23 @@
+use std::path::PathBuf;
+
+use clap::{arg, command, Parser};
+
+#[derive(Debug, Parser)]
+#[command(author, version, long_about = Some("Utility for autoformatting RON files."))]
+pub struct Arguments {
+    /// Sets which file to format
+    #[arg()]
+    pub input: PathBuf,
+
+    /// Sets soft max line width for formatting heuristics
+    #[arg(short, default_value_t = 40)]
+    pub width: usize,
+
+    /// Sets indentation size in spaces
+    #[arg(short, default_value_t = 4)]
+    pub tab_size: usize,
+
+    /// Prints output to console instead of overwriting the input file
+    #[arg(short, default_value_t = false)]
+    pub debug: bool,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,45 @@
 mod ast;
 
-use std::sync::atomic::{AtomicUsize, Ordering};
+use std::{
+    ffi::OsString,
+    sync::atomic::{AtomicUsize, Ordering},
+};
 
-use clap::clap_app;
+use arguments::Arguments;
+use clap::Parser as ClapParser;
 use pest::Parser;
 use pest_derive::*;
 
 static TAB_SIZE: AtomicUsize = AtomicUsize::new(4);
 static MAX_LINE_WIDTH: AtomicUsize = AtomicUsize::new(40);
 
+mod arguments;
+
 #[derive(Parser)]
 #[grammar = "ron.pest"]
 struct RonParser;
 
 fn main() {
-    let matches = matches();
+    let args = Arguments::parse();
 
-    let target_path = matches.value_of("INPUT").unwrap();
+    TAB_SIZE.store(args.tab_size, Ordering::SeqCst);
+    MAX_LINE_WIDTH.store(args.width, Ordering::SeqCst);
 
-    if let Some(mlw) = matches.value_of("MAX_LINE_WIDTH") {
-        let mlw: usize = str::parse(mlw).unwrap();
-        MAX_LINE_WIDTH.store(mlw, Ordering::SeqCst);
-    }
-    if let Some(ts) = matches.value_of("TAB_SIZE") {
-        let ts: usize = str::parse(ts).unwrap();
-        TAB_SIZE.store(ts, Ordering::SeqCst);
-    }
-
-    let file = std::fs::read_to_string(target_path).expect("unable to read file");
+    let file = std::fs::read_to_string(&args.input).expect("unable to read file");
 
     let ron = RonParser::parse(Rule::ron_file, &file)
         .expect("unable to parse RON")
         .next()
         .unwrap();
 
-    if matches.is_present("debug") {
+    if args.debug {
         println!("{}", ast::RonFile::parse_from(ron));
     } else {
-        std::fs::copy(target_path, format!("{}.bak", &target_path))
-            .expect("unable to create backup file");
+        let mut backup = OsString::from(&args.input);
+        backup.push(".bak");
+        std::fs::copy(&args.input, &backup).expect("unable to create backup file");
 
-        std::fs::write(target_path, format!("{}", ast::RonFile::parse_from(ron)))
+        std::fs::write(args.input, format!("{}", ast::RonFile::parse_from(ron)))
             .expect("unable to overwrite target file");
     }
-}
-
-fn matches<'a>() -> clap::ArgMatches<'a> {
-    clap_app!(app =>
-        (version: "0.1.0")
-        (author: "Anton F. <a.filippov@protonmail.com>")
-        (about: "Utility for autoformatting RON files.")
-        (@arg INPUT: +required "Sets which file to format")
-        (@arg MAX_LINE_WIDTH: -w +takes_value "Sets soft max line width for formatting heuristics")
-        (@arg TAB_SIZE: -t +takes_value "Sets indentation size in spaces")
-        (@arg debug: -d "Prints output to console instead of overwriting the input file")
-    )
-    .get_matches()
 }
